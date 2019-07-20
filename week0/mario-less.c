ccbc2178a98f4470a54c73bf6685d4796caa0b98
AkselWynter <52838821+AkselWynter@users.noreply.github.com>
2019-07-20 17:37:39 +0100

    Create mario-less.c

---
+++ week0/mario-less.c
@@ -0,0 +1,34 @@
+// short program to print a mario-esque pyramid to the screen 
+
+#include <cs50.h>
+#include <stdio.h>
+
+const char POUND = '#';
+const char BLANK = ' ';
+
+void build_pyramid(char c, int i);
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    } while (n < 1 || n > 8);
+    
+    for (int i = 1; i <= n; i++)
+    {
+        build_pyramid(BLANK, n - i);
+        build_pyramid(POUND, i);
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void build_pyramid(char c, int i)
+{
+    for (int j = 0; j < i; j++)
+    {
+        printf("%c", c);
+    }
+}
