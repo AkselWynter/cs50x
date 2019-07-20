a869538d236c78c5539dbd1eafeb993025d472d2
AkselWynter <52838821+AkselWynter@users.noreply.github.com>
2019-07-20 13:42:06 +0100

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,11 @@
+# A short program that is a dynamic take on the 'hello world' program
+# Takes in user input and welcomes the user
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string name = get_string("What is your name? ");
+    printf("hello, %s\n", name);
+}
