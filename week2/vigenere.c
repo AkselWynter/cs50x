bcadfa5c75a139a3f6888ea676e14995cdc450de
AkselWynter <52838821+AkselWynter@users.noreply.github.com>
2019-07-20 20:41:46 +0100

    Update vigenere.c

--- week2/vigenere.c
+++ week2/vigenere.c
@@ -4,23 +4,13 @@
 #include <ctype.h>
 #include <string.h>
 
-
-// add function to check if keyword argument is alpha
-bool check_validity(string keyword)
-{
-    for (int i = 0; i < strlen(keyword); i++)
-    {
-        if (!isalpha(keyword[i]))
-        {
-            return false;
-        }
-    }
-    return true;
-}
+bool check_valid(string keyword)
 
 int main(int argc, string argv[])
 {
-    if (argc != 2 || !check_validity(argv[1]))
+    // check that the correct number of cmd line arguments have been provided
+    // and that the provided keyword is valid
+    if (argc != 2 || !check_valid(argv[1]))
     {
         printf("Usage: ./viginere keyword\n");
         return 1;
@@ -32,11 +22,18 @@ int main(int argc, string argv[])
     
     for (int i = 0, j = 0; i < strlen(plaintext); i++)
     {
+        // check if each char of the plaintext is a letter and needs shifting
+        // or is non-alpha i.e. punctuation
         if (isalpha(plaintext[i]))
         {
             int ascii_shift = isupper(plaintext[i]) ? 65 : 97;
+            
+            // set current shift key based on the current letter in the keyword
+            // looping back to the beginning if plaintext is longer than the keyword
             int k = toupper(keyword[j % strlen(keyword)]) - 65;
             
+            // shift the current letter of the plaintext by the shift key and adjust
+            // depending on whether the char is upper or lowercase
             printf("%c", (((plaintext[i] - ascii_shift) + k) % 26) + ascii_shift);  
             j++;
         }
@@ -47,4 +44,17 @@ int main(int argc, string argv[])
     }
     printf("\n");
     return 0;
+}
+
+// check that the keyword provided is valid (all elements are letters not numbers etc)
+bool check_valid(string keyword)
+{
+    for (int i = 0; i < strlen(keyword); i++)
+    {
+        if (!isalpha(keyword[i]))
+        {
+            return false;
+        }
+    }
+    return true;
 }
